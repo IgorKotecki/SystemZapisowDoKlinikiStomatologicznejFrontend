d0bafbdf563179b89b624bdd5f05aafaef880f3e
s27014 <s27014@pjwstk.edu.pl>
2025-12-07 18:28:13 +0100

    Added refresh token logics

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { storage } from '../utils/storage';
+import { refreshTokenFlow } from './refreshToken';
 
 const api = axios.create({
   baseURL: 'http://localhost:5114',
@@ -16,71 +17,21 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
-//PRZEANALIZOWAĆ DODAĆ W RAZIE CO 
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// // flaga, żeby uniknąć nieskończonej pętli
-// let isRefreshing = false;
-// let failedQueue: any[] = [];
-
-// const processQueue = (error: any, token: string | null = null) => {
-//   failedQueue.forEach(prom => {
-//     if (error) {
-//       prom.reject(error);
-//     } else {
-//       prom.resolve(token);
-//     }
-//   });
-//   failedQueue = [];
-// };
-
-// interceptor odpowiedzi
 // api.interceptors.response.use(
-//   response => response,
-//   async error => {
+//   (response) => response,
+//   async (error) => {
 //     const originalRequest = error.config;
-
+    
 //     if (error.response?.status === 401 && !originalRequest._retry) {
-//       if (isRefreshing) {
-//         return new Promise((resolve, reject) => {
-//           failedQueue.push({ resolve, reject });
-//         })
-//           .then(token => {
-//             originalRequest.headers["Authorization"] = `Bearer ${token}`;
-//             return axios(originalRequest);
-//           })
-//           .catch(err => Promise.reject(err));
-//       }
-
 //       originalRequest._retry = true;
-//       isRefreshing = true;
 
-//       try {
-//         // wywołanie endpointu refresh token
-//         const refreshToken = localStorage.getItem("refreshToken");
-//         const res = await axios.post("/auth/refresh", { token: refreshToken });
-//         const newToken = res.data.accessToken;
+//       const success = await refreshTokenFlow();
 
-//         localStorage.setItem("accessToken", newToken);
-//         api.defaults.headers.common["Authorization"] = `Bearer ${newToken}`;
-//         originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+//       if (success) {
+//         const newToken = storage.getToken();
+//         originalRequest.headers.Authorization = `Bearer ${newToken}`;
 
-//         processQueue(null, newToken);
-
-//         return axios(originalRequest);
-//       } catch (err) {
-//         processQueue(err, null);
-//         return Promise.reject(err);
-//       } finally {
-//         isRefreshing = false;
+//         return api(originalRequest);
 //       }
 //     }
 
@@ -88,5 +39,6 @@ export default api;
 //   }
 // );
 
-// export default api;
+export default api;
+
 
---
+++ src/api/refreshToken.ts
@@ -0,0 +1,32 @@
+import { storage } from "../utils/storage";
+
+export async function refreshTokenFlow() {
+  const refreshToken = window.localStorage.getItem("refreshToken");
+  if (!refreshToken) return false;
+
+  try {
+    const res = await fetch("http://localhost:5114/auth/refresh", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ refreshToken }),
+    });
+
+    if (!res.ok) return false;
+
+    const data = await res.json();
+
+    const { accessToken, refreshToken: newRefresh } = data;
+
+    window.localStorage.setItem("token", accessToken);
+    window.localStorage.setItem("refreshToken", newRefresh);
+
+    storage.setToken(accessToken);
+
+    console.log("🔄 Token refreshed!");
+
+    return true;
+  } catch (err) {
+    console.error("Refresh token error:", err);
+    return false;
+  }
+}
